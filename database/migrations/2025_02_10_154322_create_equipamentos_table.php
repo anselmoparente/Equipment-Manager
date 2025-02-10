7d7c1e1d4a932e53ca9afcb344bdf8cf471e4263
Anselmo Parente Martins <anselmo.martins@unifametro.edu.br>
2025-02-10 15:47:38 -0300

    feat: Create migrations

---
+++ database/migrations/2025_02_10_154322_create_equipamentos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('equipamentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->boolean('status')->default(false);
+            $table->string('parametro');
+            $table->float('limite_min');
+            $table->float('limite_max');
+            $table->string('topic');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('equipamentos');
+    }
+};
---
+++ database/migrations/2025_02_10_154348_create_alertas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alertas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('equipamento_id')->constrained('equipamentos')->onDelete('cascade');
+            $table->float('valor');
+            $table->string('mensagem');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alertas');
+    }
+};
